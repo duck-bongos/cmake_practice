4dcb6c902204ebe3dd10f470ab543bc2467f919b
Dan Billmann <billmannd@gmail.com>
2023-01-02 13:07:07 -0500

    first steps towards CMake tutorial

---
+++ build/CMakeFiles/Toot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Toot.dir/src/main.cc.o"
+  "CMakeFiles/Toot.dir/src/main.cc.o.d"
+  "Toot"
+  "Toot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Toot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Toot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Toot.
+# This may be replaced when dependencies are built.
